f0a6588024c48de252959a6beb8e1203b06fb878
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-04-22 22:14:44 +0530

    installing nodejs in backend server

--- backend.sh
+++ backend.sh
@@ -12,10 +12,10 @@ N="\e[0m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "Installation of $2.... $R FAILURE $N"
+        echo -e "$2.... $R FAILURE $N"
         exit 1 #manual exit in case of error
     else
-        echo -e "Installation of $2....$G SUCCESS $N"
+        echo -e "$2....$G SUCCESS $N"
     fi
 }
 
