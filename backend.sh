6f994f0792e0aa7f86f3ce71432b2d94e6d8a5ae
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-04-23 10:02:33 +0530

    installing nodejs script created

--- backend.sh
+++ backend.sh
@@ -8,6 +8,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+echo "Please enter the root password for DB: "
+read -s mysql_root_password
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -44,3 +46,39 @@ then
 else
     echo -e "Expense user already added... $Y SKIPPING $N"
 fi
+
+mkdir -p /app &>>$LOGFILE
+VALIDATE $? "Created /app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
+VALIDATE $? "Downloading Backend code"
+
+cd /app 
+rm -rf /app/*
+unzip /tmp/backend.zip &>>$LOGFILE
+VALIDATE $? "Extracted (Unzipped) Backend Code in /app directory"
+
+npm install &>>$LOGFILE
+VALIDATE $? "Installing nodejs dependencies"
+
+cp /home/ec2-user/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+VALIDATE $? "Creating backend.service"
+
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "Reloading daemon"
+
+systemctl start backend &>>$LOGFILE
+VALIDATE $? "Start backend service"
+
+systemctl enable backend &>>$LOGFILE
+VALIDATE $? "Enable backend service"
+
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Install MySQL Client"
+
+mysql -h db.surya-devops.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+VALIDATE $? "Loading Schema for MySQL Client"
+
+systemctl restart backend &>>$LOGFILE
+VALIDATE $? "Restarting backend service"
